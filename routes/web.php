c149eba2bd85ceaecce4bda9aef3f1c79ddfefcb
nikoloz <ngvritishvili@hotmail.com>
2023-12-23 15:21:05 +0400

    fro revaz

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/api/docs');
+Route::get('/for/revaz', function () {
+    return view('welcome');
+//    return redirect('/api/docs');
 });
